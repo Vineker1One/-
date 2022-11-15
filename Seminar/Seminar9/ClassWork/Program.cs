5b0a3dd336101d416ed8f1ab9e2d2f0c12b673a6
Ivan <ikorovyanskiy04@mail.ru>
2022-11-15 20:36:05 +0300

    .\Seminar\

--- Seminar/Seminar9/ClassWork/Program.cs
+++ Seminar/Seminar9/ClassWork/Program.cs
@@ -1,9 +1,54 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
-void ShowNums (int n)
+// void ShowNums (int n)
+// {
+//     if(n > 1) ShowNums(n - 1);
+//     Console.Write(n + " ");
+
+// }
+
+// ShowNums(5);
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// int SumOfDigits (int n)
+// {
+//     if(n != 0) return SumOfDigits (n / 10) + n % 10;
+//     else return 0;
+// }
+
+// Console.WriteLine(SumOfDigits(1234));
+
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// void ShowTwoNums (int m, int n)
+// {
+//     if (n == m) {
+//         Console.Write(m + " ");
+//     }
+//     if(n < m) 
+//     {
+//         ShowTwoNums (m-1, n);
+//         Console.Write(m + " ");
+//     }
+//     if (n > m)
+//     {
+//         ShowTwoNums(m, n - 1);
+//         Console.Write(n + " ");
+//     }
+// }
+// ShowTwoNums(1,5);
+
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+
+double Degree (double a, double b)
 {
-    if(n > 1) ShowNums(n - 1);
-    Console.Write(n + " ");
-
+    if(b < 0) 
+    {
+        return 1/a * Degree (a, b+1);
+    }
+    if(b > 0) 
+    {
+        return a * Degree (a, b-1);
+    }
+    return 1;
 }
-
-ShowNums(5);
+Console.Write(Degree(2,-3));
---
+++ Seminar/Seminar9/HomeWork/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте значени N. Напишите программу, которая выведет все натуральные чилса от N до 1. Выполнить с помощью рекурсии.
+
+void ShowNums (int n)
+{
+    Console.Write (n + " ");
+    if (n > 1) ShowNums(n - 1);
+}
+// ShowNums(5);
+
+// Зайдайте значения M и N. Напишите программу, которая найдет сумму нарутральных элементов в промежутке от M и N.
+
+int SumNum (int m, int n)
+{
+    if(m < n)
+    {
+        return m = m + SumNum (m+1, n);
+    }
+    if(m > n)
+    {
+        return m = m + SumNum (m-1, n);
+    }
+
+    else return n;
+}
+// Console.WriteLine(SumNum(1,4));
+
+// Напиште программу для вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+Console.Write(Akkerman(3, 2));
